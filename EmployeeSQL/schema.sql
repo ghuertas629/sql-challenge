f580176612d00d166978f4771925eb4f8e0c0b1f
ghuertas629 <gh5577@alum.uncw.edu>
2020-05-04 22:22:15 -0400

    updated schema and ERD

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -2,7 +2,7 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE Departments (
+CREATE TABLE "Departments" (
     "dept_no" CHAR(4)   NOT NULL,
     "dept_name" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_Departments" PRIMARY KEY (
@@ -10,60 +10,75 @@ CREATE TABLE Departments (
      )
 );
 
-CREATE TABLE Dept_Emp (
+CREATE TABLE "Dept_Emp" (
     "emp_no" INT   NOT NULL,
     "dept_no" CHAR(4)   NOT NULL,
     "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Dept_Emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
-CREATE TABLE Dept_Manager (
+CREATE TABLE "Dept_Manager" (
     "dept_no" CHAR(4)   NOT NULL,
     "emp_no" INT   NOT NULL,
     "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Dept_Manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
-CREATE TABLE Employees (
+CREATE TABLE "Employees" (
     "emp_no" INT   NOT NULL,
     "birth_date" DATE   NOT NULL,
     "first_name" VARCHAR(255)   NOT NULL,
     "last_name" VARCHAR(255)   NOT NULL,
-    "gender" VARCHAR(1)   NOT NULL,
+    "gender" CHAR(1)   NOT NULL,
     "hire_date" DATE   NOT NULL,
     CONSTRAINT "pk_Employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
-CREATE TABLE Salaries (
+CREATE TABLE "Salaries" (
+    "salary_id" SERIAL   NOT NULL,
     "emp_no" INT   NOT NULL,
     "salary" INT   NOT NULL,
     "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "salary_id"
+     )
 );
 
-CREATE TABLE Titles (
+CREATE TABLE "Titles" (
+    "title_id" SERIAL   NOT NULL,
     "emp_no" INT   NOT NULL,
     "title" VARCHAR(255)   NOT NULL,
     "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
 );
 
-ALTER TABLE Dept_Emp ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES Employees ("emp_no");
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
 
-ALTER TABLE Dept_Emp ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES Departments ("dept_no");
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
 
-ALTER TABLE Dept_Manager ADD CONSTRAINT "fk_Dept_Manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES Departments ("dept_no");
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
 
-ALTER TABLE Dept_Manager ADD CONSTRAINT "fk_Dept_Manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES Employees ("emp_no");
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
 
-ALTER TABLE Salaries ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES Employees ("emp_no");
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
 
-ALTER TABLE Titles ADD CONSTRAINT "fk_Titles_emp_no" FOREIGN KEY("emp_no")
-REFERENCES Employees ("emp_no");
+ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
